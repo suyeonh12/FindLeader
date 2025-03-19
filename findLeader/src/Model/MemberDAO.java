178b9fb8deb7ed7e8d4b95fde5584c2432388d0e
suyeonh <suyeonh12@gmail.com>
2025-03-19 18:48:38 +0900

    250319 main edit

--- findLeader/src/Model/MemberDAO.java
+++ findLeader/src/Model/MemberDAO.java
@@ -5,10 +5,16 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Random;
+import java.util.Scanner;
 
 import javax.naming.spi.DirStateFactory.Result;
 
 public class MemberDAO {
+	
+	//필요 유틸
+	Scanner sc = new Scanner(System.in);
+	Random ran = new Random();
 
 	// DB연결 관련 초기값 세팅
 	Connection conn = null;
@@ -93,6 +99,45 @@ public class MemberDAO {
 		}
 		return result;
 	}
-
+	
+	//세번째 게임: 경비원을 이겨라!
+	public int fight() {
+		int count = 1;
+		int Playerhp = 80;
+		int Enemyhp = 80;
+		while (true) {
+			int com_choice = ran.nextInt(3) + 1;
+			System.out.println(count + "번째 턴입니다.");
+			System.out.println("경비원의 공격! 공격을 회피하세요!\n [1] 왼쪽 [2] 오른쪽");
+			int choice = sc.nextInt();
+			if (com_choice != 1) {
+				System.out.println("경비원의 공격을 피했습니다");
+			}
+			if (com_choice == 1) {
+				System.out.println("경비원의 공격을 피하지 못했습니다. '플레이어' hp -10");
+				Playerhp -= 10;
+			}
+			System.out.println("플레이어 HP : " + Playerhp + " 경비원 HP : " + Enemyhp);
+			if (Playerhp <= 0) {
+				break;
+			}
+			System.out.println("당신의 공격! 어느쪽으로 공격하시겠습니까? \n [1] 왼쪽 [2] 오른쪽");
+			choice = sc.nextInt();
+			if (com_choice != 1) {
+				System.out.println("공격이 적중했습니다! '경비원' hp - 10");
+				Enemyhp -= 10;
+			}
+			if (com_choice == 1) {
+				System.out.println("경비원이 공격을 피했습니다");
+			}
+			System.out.println("플레이어 HP : " + Playerhp + " 경비원 HP : " + Enemyhp);
+			if (Enemyhp <= 0) {
+				System.out.println("승리했습니다!");
+				break;
+			}
+			count++;
+		}
+		return Playerhp;
+	}
 
 }
